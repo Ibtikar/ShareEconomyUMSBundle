86e70616fc788b541e67e3d65c04992d182d80f2
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-07-27 13:56:11 +0200

    Enhancement: Add missing listener from the previous commit

---
+++ Listener/AuthenticationSuccessListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ibtikar\ShareEconomyUMSBundle\Listener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Ibtikar\ShareEconomyUMSBundle\Service\APIOperations;
+use Ibtikar\ShareEconomyUMSBundle\Entity\User;
+
+/**
+ * @author Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
+ */
+class AuthenticationSuccessListener
+{
+
+    /** @var APIOperations $APIOperations */
+    private $APIOperations;
+
+    /**
+     * @param APIOperations $APIOperations
+     */
+    public function __construct(APIOperations $APIOperations)
+    {
+        $this->APIOperations = $APIOperations;
+    }
+
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+        if (!$user instanceof User) {
+            return;
+        }
+        $userData = $this->APIOperations->getUserData($user);
+        foreach ($userData as $key => $value) {
+            $data[$key] = $value;
+        }
+        $event->setData($data);
+    }
+}
