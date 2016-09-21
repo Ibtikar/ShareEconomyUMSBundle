dc6d00935d2871509b381085860131f423c956c5
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-09-21 14:38:33 +0200

    Bug fix: Call the parent constructor in the child class to set the validator object

--- Service/UserOperations.php
+++ Service/UserOperations.php
@@ -27,6 +27,7 @@ class UserOperations extends APIOperations
     {
         $this->container = $container;
         $this->configParams = $container->getParameter('ibtikar.shareeconomy.ums.parameters');
+        parent::__construct($container->get('validator'));
     }
 
     /**
